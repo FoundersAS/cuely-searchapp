493926e7eb18205e6469f43d1f252a3d990f3d50
Jan Gnezda <jan@pipetop.com>
2016-11-24 15:42:36 +0100

    Fix for missing Opbeat stacktraces

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var { app } = require('electron');
 process.env.BABEL_CACHE_PATH = app.getPath('userData') + '/.babel-cache.json';
 
+require('babel-core/register');
+
 // OPBEAT
 var opbeat = require('opbeat').start({
   appId: 'dfa235a44d',
@@ -8,14 +10,5 @@ var opbeat = require('opbeat').start({
   secretToken: '7d0e3d206f858fbaeddb3ca0be74a7baf4280e9e',
 })
 
-/*
-var { opbeat } = require('opbeat-react');
-opbeat.initOpbeat({
-  orgId: '6fdeb7471e66415ca8f90f81093af2b5',
-  appId: 'dfa235a44d',
-});
-*/
-
-require('babel-core/register');
 require('babel-polyfill');
 require('./main');
