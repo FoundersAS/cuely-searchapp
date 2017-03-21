ef6681820c0ddf3b07f63a8981225ec7e5e3ed3b
Jan Gnezda <jan@pipetop.com>
2017-03-21 13:30:10 +0100

    Move opbeat ids and token to env file

--- env_TEMPLATE.js
+++ env_TEMPLATE.js
@@ -4,3 +4,7 @@ process.env.API_ROOT = 'backend_url';
 process.env.ALGOLIA_INDEX = 'index_name';
 // address of the update server
 process.env.UPDATE_FEED_URL = 'update_url';
+// opbeat
+process.env.OPBEAT_APP_ID = 'app_id';
+process.env.OPBEAT_ORG_ID = 'org_id';
+process.env.OPBEAT_TOKEN = 'token';
--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var { app } = require('electron');
 process.env.BABEL_CACHE_PATH = app.getPath('userData') + '/.babel-cache.json';
 
 require('babel-core/register');
+require('./env.js');
 
 // OPBEAT
 var opbeat = require('opbeat').start({
-  appId: 'dfa235a44d',
-  organizationId: '6fdeb7471e66415ca8f90f81093af2b5',
-  secretToken: '7d0e3d206f858fbaeddb3ca0be74a7baf4280e9e',
+  appId: process.env.OPBEAT_APP_ID,
+  organizationId: process.env.OPBEAT_ORG_ID,
+  secretToken: process.env.OPBEAT_TOKEN
 })
 
 require('babel-polyfill');
-require('./env.js');
 require('./main');
